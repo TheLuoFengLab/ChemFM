3b1055a62c4e72a67daf905911f362273bb7394c
Feiyang Cai <29678149+feiyang-cai@users.noreply.github.com>
2024-10-29 22:12:11 -0400

    Delete check.py

--- check.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-model = AutoModelForCausalLM.from_pretrained("feiyang-cai/ChemFM-1B")
-tokenizer = AutoTokenizer.from_pretrained("feiyang-cai/ChemFM-1B")
-
-print(model)
-print(tokenizer)
